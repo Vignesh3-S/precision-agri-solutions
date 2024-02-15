350b39aa2f6d5838b733895b5c974f9d3fb6c0fd
Vignesh3-S <vignesh.36dev@gmail.com>
2024-02-15 18:19:00 +0530

    updated settings

--- agriproject/settings.py
+++ agriproject/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://precision-agricultural-solution.onrender.com']
-CSRF_TRUSTED_ORIGINS = ['https://precision-agricultural-solution.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
