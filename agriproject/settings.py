8527e224b9d1513d12cb9324668a6abeb14edf48
Vignesh3-S <vignesh.36dev@gmail.com>
2024-02-15 18:35:21 +0530

    update set

--- agriproject/settings.py
+++ agriproject/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         'USER': os.getenv('USER'),
         'PASSWORD':os.getenv('PASSWORD'), 
         'HOST': os.getenv('HOST'),
-        'PORT': os.getenv('PORT'),
+        'PORT':"19446",
     }
 }
 
