d789dbb77ae42d9c3c5c0be4068a85ff5b91373f
Vignesh3-S <vignesh.36dev@gmail.com>
2024-03-09 22:57:14 +0530

    updated

--- agriproject/settings.py
+++ agriproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_bootstrap5', # bootstrap
+    'django_bootstrap5', # bootstrap 
     'precisionagri', # pas
     'django_recaptcha', # captcha 
     # for third party
